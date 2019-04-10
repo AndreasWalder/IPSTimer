5383a0edd10541cff346e4b9bb53f587d5f1f77f
Andreas Walder <andreas.walder@web.de>
2019-04-11 00:53:32 +0200

    1.

--- IPSTimer/module.php
+++ IPSTimer/module.php
@@ -15,8 +15,8 @@
         public function ApplyChanges() {
             //Never delete this line!
             parent::ApplyChanges();
-			//$triggerID = $this->GetIDForIdent("Gesetzt");
-            $triggerID = $this->ReadPropertyInteger("gesetzt");
+			$triggerID = $this->GetIDForIdent("Gesetzt");
+            //$triggerID = $this->ReadPropertyInteger("gesetzt");
             $this->RegisterMessage($triggerID, 10603 /* VM_UPDATE */);
         }
         public function MessageSink ($TimeStamp, $SenderID, $Message, $Data) {
