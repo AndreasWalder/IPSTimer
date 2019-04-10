f2133f49f12605455973fdf602e598ae94206b04
Andreas Walder <andreas.walder@web.de>
2019-04-11 00:32:33 +0200

    1.

--- IPSTimer/module.php
+++ IPSTimer/module.php
@@ -22,11 +22,10 @@
         public function MessageSink ($TimeStamp, $SenderID, $Message, $Data) {
             $triggerID = $this->ReadPropertyInteger("OutputID");
             if (($SenderID == $triggerID) && ($Message == 10603) && (boolval($Data[0]))) {
-				
-				echo boolval($Data[0]);
-				echo $SenderID;
-				echo "trigger";
-				echo $triggerID;
+				if (!GetValue($this->GetIDForIdent("gesetzt"))){
+                return;
+                }
+				this->Stop();
 				
 			}
 				if (GetValue($this->GetIDForIdent("gesetzt"))){
