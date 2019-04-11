7f46ee89d09106a6e4220e03c0152a117c7fd089
Andreas Walder <andreas.walder@web.de>
2019-04-11 22:08:59 +0200

    1.

--- IPSTimer/module.php
+++ IPSTimer/module.php
@@ -59,7 +59,7 @@
 					IPS_SetEventTriggerValue($eid, true);		                                  //Nur auf TRUE Werte auslösen
 					IPS_SetParent($eid, $this->GetIDForIdent("InputTriggerID"));                  //Ereigniss zuordnen zu Variable "gesetzt"     									  //Ereignis zuordnen
 					// Füge eine Regel mit der ID 2 hinzu: Variable "gesetzt" == true
-                    IPS_SetEventConditionVariableRule($eid, $this->GetIDForIdent("InputTriggerID"), 2, $this->GetIDForIdent("InputTriggerID"), 0, true);
+                    IPS_SetEventConditionVariableRule($eid, 2, 2, $this->GetIDForIdent("InputTriggerID"), 0, true);
 					IPS_SetEventActive($eid, true);          								      //Ereignis aktivieren
 					}
                     break;
