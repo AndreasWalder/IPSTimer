992cc2b81d91e253e7ad93ce339b2dc63e66c937
Andreas Walder <andreas.walder@web.de>
2019-04-11 23:15:28 +0200

    1.

--- IPSTimer/module.php
+++ IPSTimer/module.php
@@ -53,31 +53,31 @@
 					$EreignisID = @IPS_GetEventIDByName("IPSTimerEventAn", $this->GetIDForIdent("InputTriggerID"));
                     if ($EreignisID === false)
 					{
-					$eid = IPS_CreateEvent(0);                									  //Ausgelöstes Ereignis
-					IPS_SetEventTrigger($eid, 4, $this->ReadPropertyInteger("OutputID"));         //Bei Änderung von Variable mit ID 15754
+					$eidan = IPS_CreateEvent(0);                									  //Ausgelöstes Ereignis
+					IPS_SetEventTrigger($eidan, 4, $this->ReadPropertyInteger("OutputID"));         //Bei Änderung von Variable mit ID 15754
 					// Füge eine Regel mit der ID 2 hinzu: Variable "InputTriggerID" == true
-					IPS_SetEventCondition($eid, 0, 0, 0);
-                    IPS_SetEventConditionVariableRule($eid, 0, 2, $this->GetIDForIdent("InputTriggerID"), 0, false);
-					IPS_SetEventTriggerValue($eid, true);		                                  //Nur auf TRUE Werte auslösen
-                    IPS_SetEventTriggerSubsequentExecution($eid, true); 
-					IPS_SetParent($eid, $this->GetIDForIdent("InputTriggerID"));                  //Ereigniss zuordnen zu Variable "InputTriggerID"  
-					IPS_SetName($eid, "IPSTimerEventAn");								              //Name dem Event zuordnen
-					IPS_SetEventActive($eid, true);          								      //Ereignis aktivieren
+					IPS_SetEventCondition($eidan, 0, 0, 0);
+                    IPS_SetEventConditionVariableRule($eidan, 0, 2, $this->GetIDForIdent("InputTriggerID"), 0, false);
+					IPS_SetEventTriggerValue($eidan, true);		                                  //Nur auf TRUE Werte auslösen
+                    IPS_SetEventTriggerSubsequentExecution($eidan, true); 
+					IPS_SetParent($eidan, $this->GetIDForIdent("InputTriggerID"));                  //Ereigniss zuordnen zu Variable "InputTriggerID"  
+					IPS_SetName($eidan, "IPSTimerEventAn");								              //Name dem Event zuordnen
+					IPS_SetEventActive($eidan, true);          								      //Ereignis aktivieren
 					}
 					
 					$EreignisID = @IPS_GetEventIDByName("IPSTimerEventOFF", $this->GetIDForIdent("InputTriggerID"));
                     if ($EreignisID === false)
 					{
-					$eid = IPS_CreateEvent(0);                									  //Ausgelöstes Ereignis
-					IPS_SetEventTrigger($eid, 4, $this->ReadPropertyInteger("OutputID"));         //Bei Änderung von Variable mit ID 15754
+					$eidaus = IPS_CreateEvent(0);                									  //Ausgelöstes Ereignis
+					IPS_SetEventTrigger($eidaus, 4, $this->ReadPropertyInteger("OutputID"));         //Bei Änderung von Variable mit ID 15754
 					// Füge eine Regel mit der ID 2 hinzu: Variable "InputTriggerID" == true
-					IPS_SetEventCondition($eid, 0, 0, 0);
-                    IPS_SetEventConditionVariableRule($eid, 0, 2, $this->GetIDForIdent("InputTriggerID"), 0, true);
-					IPS_SetEventTriggerValue($eid, false);		                                  //Nur auf TRUE Werte auslösen
-                    IPS_SetEventTriggerSubsequentExecution($eid, true); 
-					IPS_SetParent($eid, $this->GetIDForIdent("InputTriggerID"));                  //Ereigniss zuordnen zu Variable "InputTriggerID"  
-					IPS_SetName($eid, "IPSTimerEventOFF");								              //Name dem Event zuordnen
-					IPS_SetEventActive($eid, true);          								      //Ereignis aktivieren
+					IPS_SetEventCondition($eidaus, 0, 0, 0);
+                    IPS_SetEventConditionVariableRule($eidaus, 0, 2, $this->GetIDForIdent("InputTriggerID"), 0, true);
+					IPS_SetEventTriggerValue($eidaus, false);		                                  //Nur auf TRUE Werte auslösen
+                    IPS_SetEventTriggerSubsequentExecution($eidaus, true); 
+					IPS_SetParent($eidaus, $this->GetIDForIdent("InputTriggerID"));                  //Ereigniss zuordnen zu Variable "InputTriggerID"  
+					IPS_SetName($eidaus, "IPSTimerEventOFF");								              //Name dem Event zuordnen
+					IPS_SetEventActive($eidaus, true);          								      //Ereignis aktivieren
 					}
 					
 					
