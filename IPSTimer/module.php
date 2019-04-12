4c3eb960163a9f2d32fc9cabd523cbe6e0a10a17
Andreas Walder <andreas.walder@web.de>
2019-04-12 16:42:50 +0200

    ""

--- IPSTimer/module.php
+++ IPSTimer/module.php
@@ -109,14 +109,14 @@
 			SetValue($this->GetIDForIdent("Dauer"), $this->ReadPropertyInteger("Duration"));
 			
 			
-			if (GetValue($this->ReadPropertyBoolean("SchalterEnable"))) {
+			if ($this->ReadPropertyBoolean("SchalterEnable")) {
 			   IPS_SetHidden($this->GetIDForIdent("Schalten"), true); //Objekt verstecken
 			}
 			else {
 			   IPS_SetHidden($this->GetIDForIdent("Schalten"), false); //Objekt anzeigen
 			}
 			
-			if (GetValue($this->ReadPropertyBoolean("TasterEnable"))) {
+			if ($this->ReadPropertyBoolean("TasterEnable")) {
 			   IPS_SetHidden($this->GetIDForIdent("Taster"), true); //Objekt verstecken
 			}
 			else {
